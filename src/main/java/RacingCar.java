f19a4e6807c8c8f63eea5089f6e386a914e6d28f
seungy0 <dltmdrb98@gmail.com>
2024-03-25 15:06:36 +0900

    feat: 랜덤값에 따라 자동차 전진하거나 멈추는 기능 구현

--- src/main/java/RacingCar.java
+++ src/main/java/RacingCar.java
@@ -1,6 +1,18 @@
+import java.util.Random;
+
 public class RacingCar {
+
+    public boolean getNumAndMove() {
+        int num = getRandomNum();
+        return move(num);
+    }
 
     public boolean move(int num) {
         return num >= 4 && num <= 9;
+    }
+
+    private static int getRandomNum() {
+        Random random = new Random();
+        return random.nextInt(9);
     }
 }
